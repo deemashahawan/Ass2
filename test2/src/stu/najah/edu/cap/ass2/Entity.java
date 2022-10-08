d0d7c94909637cda3e287b179ee0fc9b69249e7a
deemashahawan <dshahwan42@gmail.com>
2022-10-08 19:34:29 +0100

    ass2

--- test2/src/stu/najah/edu/cap/ass2/Entity.java
+++ test2/src/stu/najah/edu/cap/ass2/Entity.java
@@ -1,8 +1,23 @@
 package stu.najah.edu.cap.ass2;
 
- public class Entity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Entity {
 
+      public ArrayList<Monster> getmonsters() {
+          return monsters;
+      }
+    public ArrayList<Player> getplayers() {
+        return players;
+    }
+      private ArrayList<Monster> monsters;
+    private ArrayList<Player> players;
+
+      public Entity(){
+          monsters = new ArrayList<>();
+          players=new ArrayList<>();
+      }
 
      public void move() {
          System.out.println("Entity movement ");
--- test2/src/stu/najah/edu/cap/ass2/Main.java
+++ test2/src/stu/najah/edu/cap/ass2/Main.java
@@ -2,21 +2,14 @@ package stu.najah.edu.cap.ass2;
 
 public class Main {
     public static void main(String[] args) {
-        Monster e1=new Monster();
-       e1.move();
-       e1.render();
-       e1.noise();
-       MonsterFast e2=new MonsterFast();
-        e2.move();
-        e2.render();
-        e2.noise();
-        MonsterStealth e3=new MonsterStealth();
-        e3.move();
-        e3.render();
-        e3.noise();
 
-        Player p=new Player();
-        p.move();
-        p.render();
+        Entity e=new Entity();
+        System.out.println("new player: " );
+        e.getplayers().add(new Player());
+        e.getmonsters().add(new Monster());
+
+
+
+
     }
 }
